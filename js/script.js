d1115493f576d36e5e05a8adde13b4e6e80a2ddd
AlessandroRamoni <alessandroramoni@yahoo.it>
2022-08-31 16:28:08 +0200

    working on odd or even

--- js/script.js
+++ js/script.js
@@ -1,9 +1,12 @@
 console.log("Mercoledì");
 
+////////////////////// PALINDROMA
+
+/*
 const word = prompt("Inserisci la prima parola che ti viene in mente");
 console.log(word);
 
-var parolaInvertita = reverseWord(word);
+const parolaInvertita = reverseWord(word);
 console.log(parolaInvertita);
 
 if (parolaInvertita === word) {
@@ -15,4 +18,21 @@ if (parolaInvertita === word) {
 function reverseWord(word) {
   const parolaAlContrario = word.split("").reverse().join("");
   return parolaAlContrario;
+}
+*/
+/////////////////// PARI E DISPARI
+
+let opzione = true;
+console.log(typeof opzione);
+
+if (opzione === true) {
+  const scelta = parseInt(prompt("Inserisci un numero da 1 a 5"));
+  console.log(scelta);
+  const numeroCpu = Math.floor(Math.random() * 5) + 1;
+  console.log(numeroCpu);
+  const somma = scelta + numeroCpu;
+  console.log(somma);
+  if (somma % 2 === 0) {
+  }
+} else {
 }
