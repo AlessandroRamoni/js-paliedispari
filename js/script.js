be3b80f7d73717845c6a145d8ea7f015800f6ba3
AlessandroRamoni <alessandroramoni@yahoo.it>
2022-08-31 15:55:17 +0200

    create function "reverseword"

--- js/script.js
+++ js/script.js
@@ -1,1 +1,18 @@
 console.log("Mercoledì");
+
+const word = prompt("Inserisci la prima parola che ti viene in mente");
+console.log(word);
+
+var parolaInvertita = reverseWord(word);
+console.log(parolaInvertita);
+
+if (parolaInvertita === word) {
+  console.log("La parola è palindroma");
+} else {
+  console.log("La parola non è palindroma");
+}
+
+function reverseWord(word) {
+  const parolaAlContrario = word.split("").reverse().join("");
+  return parolaAlContrario;
+}
